58c5643f7ad5e13aeb1bbeff1a099c40f02fe8bc
Wasted_Marl <ilu.raghus@icloud.com>
2024-09-05 11:17:55 -0600

    Verfied with x+2

--- regula-falsi.py
+++ regula-falsi.py
@@ -1,4 +1,19 @@
-def regulafalsi(){
+def regulafalsi():
     # Function to find the root of the equation
-    
-}
+    def eq(x):
+        return x**3 - x -1
+    #initial Values
+    x0=1
+    x1=2
+    e=0.0001
+    while (x1-x0) >= e:
+        x2 = x0 - (eq(x0) * (x1 - x0)) / (eq(x1) - eq(x0))
+        if eq(x2) == 0.0:
+            break
+        else:
+            x0 = x1
+            x1 = x2
+    #have to solve by hand in order to determine which value to return and when to stop. I guess x2 for now since that is the one
+    #that converges? I donno
+    return x2
+print(regulafalsi())
