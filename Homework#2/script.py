d1b9cd48a686b6f80a08c443015bc166b8eb8e28
Wasted_Marl <ilu.raghus@icloud.com>
2024-10-09 11:05:29 -0600

    Homework2

--- Homework#2/script.py
+++ Homework#2/script.py
@@ -1,29 +1,28 @@
-# import numpy as np
+import numpy as np
 
-# def gauss_seidel(A, b, tol=0.05, max_iterations=100):
-#     x = np.zeros(len(b))
-#     n = len(b)
-#     for iteration in range(max_iterations):
-#         x_old = x.copy()
-#         for i in range(n):
-#             sum_ax = sum(A[i][j] * x[j] for j in range(n) if j != i)
-#             x[i] = (b[i] - sum_ax) / A[i][i]
-#         # Compute relative error
-#         relative_error = np.max(np.abs((x - x_old) / x))
-#         if relative_error < tol:
-#             break
-#     return x, iteration+1
+def gauss_seidel(A, b, tol=0.05, max_iterations=100):
+    x = np.zeros(len(b))
+    n = len(b)
+    for iteration in range(max_iterations):
+        x_old = x.copy()
+        for i in range(n):
+            sum_ax = sum(A[i][j] * x[j] for j in range(n) if j != i)
+            x[i] = (b[i] - sum_ax) / A[i][i]
+        # Compute relative error
+        relative_error = np.max(np.abs((x - x_old) / x))
+        if relative_error < tol:
+            break
+    return x, iteration+1
 
-# A = np.array([[10, 2, -1], [-3, -6, 2], [1, 1, 5]], dtype=float)
-# b = np.array([27, -61.5, -21.5], dtype=float)
+A = np.array([[10, 2, -1], [-3, -6, 2], [1, 1, 5]], dtype=float)
+b = np.array([27, -61.5, -21.5], dtype=float)
 
-# solution, iterations = gauss_seidel(A, b)
-# print(f"Solution: {solution}")
-# print(f"Iterations: {iterations}")
+solution, iterations = gauss_seidel(A, b)
+print(f"Solution: {solution}")
+print(f"Iterations: {iterations}")
 
 
 #function to find Eigen values of 3x3 matrix
-import numpy as np
 
 def eigen_values(A):
     # compute eigenvalues
@@ -31,7 +30,7 @@ def eigen_values(A):
     return eigenvalues
 A = np.array([[20, 3, 2], [3, 9, 4], [2, 4, 12]], dtype=float)
 eigenvalues = eigen_values(A)
-print(f"Eigenvalues: {eigenvalues}")
+print(f" The Eigenvalues: {eigenvalues}")
 
 def power_method(A, num_iterations):
     #randdomizing for showing improvement over iteratiosnn,. 
@@ -55,8 +54,8 @@ A = np.array([[20, 3, 2],
 # Power method to find the largest eigenvalue
 num_iterations = 20
 largest_eigenvalue, _ = power_method(A, num_iterations)
-print(f"Largest eigenvalue (Power method): {largest_eigenvalue}")
+print(f"Largest eigenvalue Estimation(Power method): {largest_eigenvalue}")
 
 # Inverse power method to find the smallest eigenvalue
 smallest_eigenvalue, _ = inverse_power_method(A, num_iterations)
-print(f"Smallest eigenvalue (Inverse power method): {smallest_eigenvalue}")
+print(f"Smallest eigenvalue Estimation(Inverse power method): {smallest_eigenvalue}")
